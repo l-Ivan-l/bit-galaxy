9ddf669e3829a717275a003dab94f77fa42bb99d
Iván Almanza <ivan.almanza.g@gmail.com>
2019-02-23 14:19:20 -0600

    Movimiento Personaje

---
+++ Bit Galaxy/Assets/Scripts/PlanetScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetScript : MonoBehaviour
+{
+    private float gravity = -25f;
+
+    public void Attract(Transform body, Rigidbody2D physxBody) {
+      Vector3 gravityUp = (body.position - transform.position).normalized;
+      Vector3 bodyUp = body.up;
+      physxBody.AddForce(gravityUp * gravity);
+      Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+      body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+    }
+
+}//class
---
+++ Bit Galaxy/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public PlanetScript planet;
+    private Rigidbody2D playerBody;
+    private Transform playerTransform;
+
+    private float moveSpeed = 10f;
+    private Vector2 moveDir;
+
+    void Awake() {
+      playerTransform = transform;
+      playerBody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+      playerBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+      playerBody.gravityScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      planet.Attract(playerTransform, playerBody);
+      PlayerController();
+    }
+
+    void FixedUpdate() {
+      Vector3 playerPosition = new Vector3(playerBody.position.x, playerBody.position.y, 0f);
+      playerBody.MovePosition(playerPosition + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+    }
+
+    void PlayerController() {
+      moveDir = new Vector2(Input.GetAxisRaw("Horizontal"), 0f).normalized;
+    }
+
+}//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//space
